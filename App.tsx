dd7707557c878adcf4cd852428ef46a759be4bf1
Xumrett <vozangel77@gmail.com>
2022-08-15 21:04:09 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+       
+        
+      <h1>©MarketInc.</h1>
+
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-qb5is6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qb5is6)
